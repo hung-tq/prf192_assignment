479db170cd1a03f429b23d15c3dcfcaf8e950a21
Hung Tran <tqhung0105@gmail.com>
2024-02-03 00:06:28 +0700

    add function 1

--- main.h
+++ main.h
@@ -7,6 +7,7 @@ void main_menu_display(); // Display main menu
 void product_menu(); // Display product menu
 void customer_menu(); // Display customer menu
 int main_menu_choice();
+void display_all_items(int item_no[], char* item_name[], int item_quantity[], int item_price[], char* item_manufactoring_date[], char* item_expiry_date[],int items); // Display all items in the inventory
 
 #endif
 
