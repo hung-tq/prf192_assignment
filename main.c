23c3ce4be1669aafafbb853526ebb9c8d27d972f
Quốc Hùng <tqhung0105@gmail.com>
2024-02-02 09:00:05 +0700

    Update main.c with docs.
    Update main.c with docs for each function.

--- main.c
+++ main.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void banner{
-
-}
-
-void main_menu{
-
-}
-
-void product_menu{
-
-}
-
-void customer_menu{
-
-}
+// Function protype
+void banner(); // Display banner
+void main_menu(); // Display main menu
+void product_menu(); // Display product menu
+void customer_menu(); // Display customer menu
 
 int main() {
     return 0;
+}
+
+// Function 
+void banner() {
+
+}
+
+void main_menu() {
+
+}
+
+void product_menu() {
+
+}
+
+void customer_menu() {
+
 }
