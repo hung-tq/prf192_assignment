b404555bcde6b40e21e822ecf298bb847bb1d597
Hung Tran <tqhung0105@gmail.com>
2024-02-02 23:17:33 +0700

    add main_menu_display

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 
 // Main program
 int main() {
+    main_menu_display();
+    int choice = main_menu_choice();
 
     return 0;
 }
@@ -13,8 +15,29 @@ void banner() {
 
 }
 
-void main_menu() {
+void main_menu_display() {
+    printf("*******************************************************************************************\n");
+    printf("*                                       Mini Market                                       *\n");
+    printf("*******************************************************************************************\n");
+    printf("*                                                                                         *\n");
+    printf("*                                  1. Display all items.                                   *\n");
+    printf("*                                  2. Add an item.                                         *\n");
+    printf("*                                  3. Sort all items.                                      *\n");
+    printf("*                                  4. Look for an item.                                    *\n");
+    printf("*                                  5. Export data to a text file.                          *\n");
+    printf("*                                  6. Delete matched item.                                 *\n");
+    printf("*                                  7. Delete all items.                                    *\n");
+    printf("*                                  8. Delete all items.                                    *\n");
+    printf("********************************************************************************************\n");
+    printf("\n                          Enter your choice:     ");
+}
 
+int main_menu_choice() {
+    int choice;
+    scanf("%d", &choice);
+    if (choice < 1 || choice > 8)
+        return -1;
+    return choice;
 }
 
 void product_menu() {
--- main.h
+++ main.h
@@ -3,9 +3,10 @@
 
 // Function protypes
 void banner(); // Display banner
-void main_menu(); // Display main menu
+void main_menu_display(); // Display main menu
 void product_menu(); // Display product menu
 void customer_menu(); // Display customer menu
+int main_menu_choice();
 
 #endif
 
