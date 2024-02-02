42a85a87b4fba2386fecd6cf480cd54bd8401f1b
Hung Tran <tqhung0105@gmail.com>
2024-02-02 22:50:41 +0700

    add header file

--- main.c
+++ main.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// Function protypes
-void banner(); // Display banner
-void main_menu(); // Display main menu
-void product_menu(); // Display product menu
-void customer_menu(); // Display customer menu
+#include "main.h"
 
 // Main program
 int main() {
+
     return 0;
 }
 
---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+// Function protypes
+void banner(); // Display banner
+void main_menu(); // Display main menu
+void product_menu(); // Display product menu
+void customer_menu(); // Display customer menu
+
+#endif
+
