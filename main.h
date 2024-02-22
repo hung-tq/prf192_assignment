00e54df1ee5fc2297ae6e6cf986efdd57f4da03a
Hung Tran <tqhung0105@gmail.com>
2024-02-22 19:52:54 +0700

    update

--- main.h
+++ main.h
@@ -15,19 +15,19 @@ void controlMenu();
 void productMenuDisplay();
 void customerMenuDisplay();
 
-int  countOfDatabase(char databaseParameter[MAXDB][MAXDB][MAXDB]);
-int databaseInput(int databaseType, int index, int lastIndex, char databaseParameter[MAXDB][MAXDB][MAXDB], char option[5], char databaseCatalogueParameter[2][5][20]);
-void databaseOutputDisplay(int databaseType, int lastIndex, char databaseParameter[MAXDB][MAXDB][MAXDB], char databaseCatalogueParameter[2][5][20]);
-void databaseCatalogueDisplay(int databaseType, char databaseCatalogueParameter[2][5][20]);
-void databaseRowSwap(int firstIndex, int secondIndex, char databaseParameter[MAXDB][MAXDB][MAXDB]);
-int databaseSortByField(int databaseType, int choiceSort, int lastIndex, char databaseParameter[MAXDB][MAXDB][MAXDB]);
-int databaseSearchByField(int databaseType, int choiceSearch, int lastIndex, int databaseSearchIndex[MAXDB], char databaseParameter[MAXDB][MAXDB][MAXDB], char databaseCatalogue[2][5][20]);
-void databaseResultListDisplay(int databaseType, int lastIndex, int databaseSearchIndex[MAXDB], char databaseParameter[MAXDB][MAXDB][MAXDB], char databaseCatalogue[2][5][20]);
-int databaseExportToFile(int databaseType, int lastIndex, char databaseParameter[MAXDB][MAXDB][MAXDB], char databaseCatalogue[2][5][20]);
-int databaseFileOpen(int databaseType, char isTerminal);
-int databaseFoundItem(int databaseSearchIndex[MAXDB]);
-int databaseDeleteMatchedItem(int databaseType, int lastIndex, int databaseSearchIndex[MAXDB], char databaseParameter[MAXDB][MAXDB][MAXDB]);
-void databaseDeleteAll(int databaseType, int lastIndex, char databaseParameter[MAXDB][MAXDB][MAXDB]);
+int  countOfDatabase            (char databaseParameter[MAXDB][MAXDB][MAXDB]);
+int  databaseInput              (int databaseType, int index, int lastIndex, char databaseParameter[MAXDB][MAXDB][MAXDB], char option[5], char databaseCatalogueParameter[2][5][20]);
+void databaseOutputDisplay      (int databaseType, int lastIndex, char databaseParameter[MAXDB][MAXDB][MAXDB], char databaseCatalogueParameter[2][5][20]);
+void databaseCatalogueDisplay   (int databaseType, char databaseCatalogueParameter[2][5][20]);
+void databaseRowSwap            (int firstIndex, int secondIndex, char databaseParameter[MAXDB][MAXDB][MAXDB]);
+int  databaseSortByField        (int databaseType, int choiceSort, int lastIndex, char databaseParameter[MAXDB][MAXDB][MAXDB]);
+int  databaseSearchByField      (int databaseType, int choiceSearch, int lastIndex, int databaseSearchIndex[MAXDB], char databaseParameter[MAXDB][MAXDB][MAXDB], char databaseCatalogue[2][5][20]);
+void databaseResultListDisplay  (int databaseType, int lastIndex, int databaseSearchIndex[MAXDB], char databaseParameter[MAXDB][MAXDB][MAXDB], char databaseCatalogue[2][5][20]);
+int  databaseExportToFile       (int databaseType, int lastIndex, char databaseParameter[MAXDB][MAXDB][MAXDB], char databaseCatalogue[2][5][20]);
+int  databaseFileOpen           (int databaseType, char isTerminal);
+int  databaseFoundItem          (int databaseSearchIndex[MAXDB]);
+int  databaseDeleteMatchedItem  (int databaseType, int lastIndex, int databaseSearchIndex[MAXDB], char databaseParameter[MAXDB][MAXDB][MAXDB]);
+void databaseDeleteAll          (int databaseType, int lastIndex, char databaseParameter[MAXDB][MAXDB][MAXDB]);
 
 
 
