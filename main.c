e5793901ed4f1f07e710d76997195334c009889d
Hung Tran <tqhung0105@gmail.com>
2024-03-20 22:31:42 +0700

    another bug =)))

--- main.c
+++ main.c
@@ -32,44 +32,44 @@ int main() {
     char databaseMain[2][MAXDB][5][MAXDB]          
     = 
     {
-    // Product Database
-    {      
-        {"chicken",    "10", "30",  "22/10/2023", "22/10/2024"},
-        {"beef",       "15", "20",  "15/11/2023", "15/11/2024"},
-        {"pork",       "12", "25",  "18/12/2023", "18/12/2024"},
-        {"fish",       "8",  "40",  "05/09/2023", "05/09/2024"},
-        {"vegetables", "5",  "50",  "30/11/2023", "30/11/2024"},
-        {"fruits",     "6",  "60",  "12/12/2023", "12/12/2024"},
-        {"bread",      "3",  "70",  "01/01/2023", "01/01/2024"},
-        {"milk",       "2",  "80",  "02/02/2023", "02/02/2024"},
-        {"cheese",     "4",  "90",  "03/03/2023", "03/03/2024"},
-        {"eggs",       "1",  "100", "04/04/2023", "04/04/2024"}
-    },
+        // Product Database
+        {      
+            {"chicken",    "10", "30",  "22/10/2023", "22/10/2024"},
+            {"beef",       "15", "20",  "15/11/2023", "15/11/2024"},
+            {"pork",       "12", "25",  "18/12/2023", "18/12/2024"},
+            {"fish",       "8",  "40",  "05/09/2023", "05/09/2024"},
+            {"vegetables", "5",  "50",  "30/11/2023", "30/11/2024"},
+            {"fruits",     "6",  "60",  "12/12/2023", "12/12/2024"},
+            {"bread",      "3",  "70",  "01/01/2023", "01/01/2024"},
+            {"milk",       "2",  "80",  "02/02/2023", "02/02/2024"},
+            {"cheese",     "4",  "90",  "03/03/2023", "03/03/2024"},
+            {"eggs",       "1",  "100", "04/04/2023", "04/04/2024"}
+        },
 
-    // Customer Database
-    {      
-        {"John",    "25", "1234567890", "1000", "50"},
-        {"Alice",   "30", "9876543210", "2000", "100"},
-        {"Bob",     "35", "5555555555", "1500", "75"},
-        {"Emma",    "28", "9999999999", "1800", "90"},
-        {"Michael", "40", "1111111111", "1200", "60"},
-        {"Sophia",  "32", "4444444444", "2500", "125"},
-        {"William", "27", "6666666666", "900",  "45"},
-        {"Olivia",  "31", "2222222222", "1700", "85"},
-        {"James",   "29", "8888888888", "1400", "70"},
-        {"Emily",   "33", "3333333333", "2200", "110"}
-    }
+        // Customer Database
+        {      
+            {"John",    "25", "1234567890", "1000", "50"},
+            {"Alice",   "30", "9876543210", "2000", "100"},
+            {"Bob",     "35", "5555555555", "1500", "75"},
+            {"Emma",    "28", "9999999999", "1800", "90"},
+            {"Michael", "40", "1111111111", "1200", "60"},
+            {"Sophia",  "32", "4444444444", "2500", "125"},
+            {"William", "27", "6666666666", "900",  "45"},
+            {"Olivia",  "31", "2222222222", "1700", "85"},
+            {"James",   "29", "8888888888", "1400", "70"},
+            {"Emily",   "33", "3333333333", "2200", "110"}
+        }
     };
 
     // Catalogue Databases: 2 tables - 5 propertie names - 20 characters length of string
     char databaseCatalogue[2][5][20] 
     =
     {
-    // Product Database Catalogue
-    {"name", "stock", "price",       "manufacturing date", "expiry date"},
+        // Product Database Catalogue
+        {"name", "stock", "price",       "manufacturing date", "expiry date"},
 
-    // Customer Database Catalogue
-    {"name", "age",   "phone number","acconut balance",    "bonus point"}, 
+        // Customer Database Catalogue
+        {"name", "age",   "phone number","acconut balance",    "bonus point"}, 
     };
 
     // Store the boolen of search result - parrallel with the databaseMain
@@ -220,13 +220,13 @@ int databaseInput(int databaseType, int index, int lastIndex, char databaseParam
             }
 
             // Check if the input string is digits larger than 0 or not, or is a true date string
-            if (option[i] - 49 == 3 && ((verifyDateStringInput_ddmmyyyy(tempHolder[i]) == -1 && databaseType == 0) || (atoi(tempHolder[i]) < 0 && databaseType == 1)) && strlen(tempHolder[i]) == 10)
+            if (option[i] - 49 == 3 && ((verifyDateStringInput_ddmmyyyy(tempHolder[i]) == -1 && databaseType == 0 && strlen(tempHolder[i]) == 10) || (atoi(tempHolder[i]) < 0 && databaseType == 1)))
             {
                 return -1; // Invalid
             }
 
             // Check if the input string is digits larger than 0 or not, or is a true date string
-            if (option[i] - 49 == 4 && ((verifyDateStringInput_ddmmyyyy(tempHolder[i]) == -1 && databaseType == 0) || (atoi(tempHolder[i]) < 0 && databaseType == 1)) && strlen(tempHolder[i]) == 10)
+            if (option[i] - 49 == 4 && ((verifyDateStringInput_ddmmyyyy(tempHolder[i]) == -1 && databaseType == 0 && strlen(tempHolder[i]) == 10) || (atoi(tempHolder[i]) < 0 && databaseType == 1)))
             {
                 return -1; // Invalid
             }
